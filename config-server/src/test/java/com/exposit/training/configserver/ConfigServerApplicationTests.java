19add6b05372aaa5c6b0ac94d52b4aa71f9f49dd
MWladislav <dalvmatujzo@gmail.com>
2020-07-09 15:24:21 +0300

    Added config server and local git repo with configuration files

---
+++ config-server/src/test/java/com/exposit/training/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exposit.training.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
